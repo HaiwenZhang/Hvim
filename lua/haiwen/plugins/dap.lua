43cba9dd7c3153dc67d546aea224d64e8c12daa6
HaiwenZhang <haiwensummer@gmail.com>
2022-11-30 23:48:26 +0800

    Add Vim use guide notes

--- lua/haiwen/plugins/dap.lua
+++ lua/haiwen/plugins/dap.lua
@@ -27,7 +27,7 @@ dapui.setup {
   },
   -- Expand lines larger than the window
   -- Requires >= 0.7
-  expand_lines = vim.fn.has "nvim-0.7" == 1,
+  -- expand_lines = vim.fn.has "nvim-0.7" == 1,
   -- Layouts define sections of the screen to place windows.
   -- The position can be "left", "right", "top" or "bottom".
   -- The size specifies the height/width depending on position. It can be an Int
@@ -41,8 +41,8 @@ dapui.setup {
         -- Elements can be strings or table with id and size keys.
         { id = "scopes", size = 0.25 },
         "breakpoints",
-        "stacks",
-        "watches",
+        -- "stacks",
+        -- "watches",
       },
       size = 40, -- 40 columns
       position = "right",
